5c1019c9f21ca4e5e9ae07282c57ab89285c7f3e
Your Name <you@example.com>
2026-05-07 22:22:31 +0530

    updated

--- src/pages/MoreInfoPage.jsx
+++ src/pages/MoreInfoPage.jsx
@@ -7,6 +7,9 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "axios";
 import LoadingMoreInfo from "../components/loadingCompo/LoadingMoreInfo";
 import { MenuLogicContext } from "../context/menuLogicContext";
+import sendImg from "../assets/send.png";
+import blackHeartPng from "../assets/black-heart.png";
+import redHeartPng from "../assets/red-heart.png";
 
 const MoreInfoPage = () => {
   // menu ko close krne k liye jab bhi more info page open ho to menu close ho jaye
@@ -77,6 +80,32 @@ const MoreInfoPage = () => {
    
   };
 
+  //listed time dikhaen ke liye ye logic 
+ const [listed, setListed] = useState("");
+
+useEffect(() => {
+
+  const oldTime = new Date(data.listed || moreInfoDataBackend.listed);
+  const now = new Date();
+
+  const diff = now - oldTime;
+
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+  if (days > 0) {
+    setListed(days + " day ago");
+  } 
+  else if (hours > 0) {
+    setListed(hours + " hour ago");
+  } 
+  else {
+    setListed(minutes + " min ago");
+  }
+
+}, []);
+
   return (
     <div className="moreInfoPage">
       <Header />
@@ -108,7 +137,19 @@ const MoreInfoPage = () => {
 
         {/* RIGHT */}
         <div className="detailsSection">
-
+           <div className="toolsDiv">
+            <div className="toolDiv">
+              <img src={blackHeartPng} alt="sendPng" />
+              <p>Favorite</p>
+            </div>
+            <div className="toolDiv">
+              <img src={sendImg} alt="sendPng" />
+              <p>send</p>
+            </div>
+            <div className="toolDiv">
+              <p>Listed : {listed}</p>
+            </div>
+           </div>
           <h2 className="name">
             {data.name || moreInfoDataBackend.brandName}
           </h2>
